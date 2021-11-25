f312e539d78be6c2a4cd52941e702e3a70f39dbe
snlucas <lucas.lambda.101@gmail.com>
2021-11-25 00:33:53 -0300

    Update project with API app

--- test_cross_commerce/asgi.py
+++ test_cross_commerce/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_cross_commerce.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_cross_commerce.settings")
 
 application = get_asgi_application()
