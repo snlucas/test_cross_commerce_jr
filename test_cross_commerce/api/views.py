f3f847b32cf3993cdec90b4af320490cb6bcba49
snlucas <lucas.lambda.101@gmail.com>
2021-11-25 23:44:31 -0300

    improve api requests with async lib

--- test_cross_commerce/api/views.py
+++ test_cross_commerce/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-import requests
+import aiohttp
+import asyncio
 
 from test_cross_commerce.utils.quick_sort import quick_sort
 
@@ -15,7 +16,7 @@ class ListData(APIView):
         Return a list with the whole data.
         """
         # Received data
-        data = self.extract_api_data().values()
+        data = asyncio.run(self.extract_api_data()).values()
         # Extract values to a list
         data = list(data)
         # Merge lists into one
@@ -29,26 +30,24 @@ class ListData(APIView):
 
         return Response(response)
 
-    def extract_api_data(self) -> dict:
+
+    async def extract_api_data(self) -> dict:
         """
         Return a dict of each page data
         """
-        # Improve requests spending time
-        session = requests.Session()
         page_data = {}
         i = 1
-
-        # If KeyError exception -> means we're on the last page.
-        # So, return the whole data.
-        try:
-            while True:
-                # Request remaining pages data
-                url = f"{self.url}{i}"
-                r = session.get(url).json()
-
-                # Feeds hash with data from each page
-                page_data[f"page{i}"] = r["numbers"]
-
-                i += 1  # Next Page
-        except KeyError:
-            return page_data
+        async with aiohttp.ClientSession() as session:
+            # If KeyError exception -> means we're on the last page.
+            # So, return the whole data.
+            try:
+                while True:
+                    # Request remaining pages data
+                    url = f"{self.url}{i}"
+                    async with session.get(url) as resp:
+                        # Feeds hash with data from each page
+                        data = await resp.json()
+                        page_data[f"page{i}"] = data['numbers']
+                    i += 1  # Next Page
+            except KeyError:
+                return page_data
